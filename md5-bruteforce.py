d67f0c6fd0da138e2d81de09893440ba0980c75d
Ryan Zumstein <rzumstein@users.noreply.github.com>
2018-06-12 21:32:59 -0500

    converting code to python3

--- md5-bruteforce.py
+++ md5-bruteforce.py
@@ -1,4 +1,4 @@
-#usr/bin/python2.7
+#usr/bin/python
 import time
 import itertools, string
 import hashlib
@@ -16,7 +16,7 @@ info = """
 """
 done = False
 def signal_handler(signal, frame):
-    print 'You pressed Ctrl+C!'
+    print('You pressed Ctrl+C!')
     global done
     done=True
     sys.exit(0)
@@ -32,49 +32,42 @@ def animate():
 
 
 def _attack(chrs, inputt):
-    print "[+] Start Time: ", time.strftime('%H:%M:%S')
+    print("[+] Start Time: ", time.strftime('%H:%M:%S'))
     start_time = time.time()
     t = threading.Thread(target=animate)
     t.start()
     total_pass_try=0
     for n in range(1, 31+1):
       characterstart_time = time.time()
-      print "\n[!] I'm at ", n , "-character"
-      
+      print("\n[!] I'm at ", n , "-character")
       
       for xs in itertools.product(chrs, repeat=n):
-
           saved = ''.join(xs)
           stringg = saved
           m = hashlib.md5()
-          m.update(saved)
+          m.update(bytes(saved, encoding='utf-8'))
           total_pass_try +=1
           if m.hexdigest() == inputt:
               time.sleep(10)
               global done
               done = True
 
-              print "\n[!] found ", stringg
-              print "\n[-] End Time: ", time.strftime('%H:%M:%S')
-              print "\n[-] Total Keyword attempted: ", total_pass_try
+              print("\n[!] found ", stringg)
+              print("\n[-] End Time: ", time.strftime('%H:%M:%S'))
+              print("\n[-] Total Keyword attempted: ", total_pass_try)
               print("\n---Md5 cracked at %s seconds ---" % (time.time() - start_time))
               sys.exit("Thank You !")
-
         
-      print "\n[!]",n,"-character finished in %s seconds ---" % (time.time() - characterstart_time)
-   
-
+      print("\n[!]",n,"-character finished in %s seconds ---" % (time.time() - characterstart_time))
 
 def main():
-    print info
+    print(info)
     
-    inp_usr = raw_input(" add md5\n")
+    inp_usr = input(" add md5\n")
     chrs = string.printable.replace(' \t\n\r\x0b\x0c', '')
-    print chrs
+    print(chrs)
     signal.signal(signal.SIGINT, signal_handler)
-    return _attack( chrs,inp_usr );
-
-   
+    return _attack( chrs,inp_usr )
 
 if __name__ == "__main__":
     main()
